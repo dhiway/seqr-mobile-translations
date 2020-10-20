e915e776c0a958ba321d8454d78367c75ba219d3
Amar Tumballi <amar@dhiway.com>
2020-10-20 16:24:04 +0530

    Update index.ts file with available translations
    Signed-off-by: Amar Tumballi <amar@dhiway.com>

--- index.ts
+++ index.ts
@@ -1,9 +1,53 @@
 import en from "./locale/English.json";
 import kn from "./locale/Kannada.json";
 import ml from "./locale/Malayalam.json";
+import ta from "./locale/Tamil.json";
+import te from "./locale/Telugu.json";
+import hi from "./locale/Hindi.json";
+import bn from "./locale/Bengali.json";
+import fr from "./locale/French.json";
+import de from "./locale/German.json";
+import gu from "./locale/Gujarati.json";
+import he from "./locale/Hebrew.json";
+import it from "./locale/Italian.json";
+import ja from "./locale/Japanese.json";
+import ko from "./locale/Korean.json";
+import ms from "./locale/Malay.json";
+import mr from "./locale/Marathi.json";
+import pa from "./locale/Punjabi.json";
+import pt from "./locale/Portuguese.json";
+import ru from "./locale/Russian.json";
+import si from "./locale/Sindi.json";
+import sl from "./locale/Sinhalese.json";
+import es from "./locale/Spanish.json";
+import sw from "./locale/Swahili.json";
+import ur from "./locale/Urdu.json";
+import vi from "./locale/Vietnamese.json";
 
 export default {
   en,
   kn,
   ml,
+  ta,
+  te,
+  hi,
+  bn,
+  fr,
+  de,
+  gu,
+  he,
+  it,
+  ja,
+  ko,
+  ms,
+  mr,
+  pa,
+  pt,
+  es,
+  sl,
+  si,
+  ru,
+  sw,
+  ur,
+  vi,
 };
